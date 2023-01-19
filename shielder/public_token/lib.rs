d05739258cf2bc7b17b00c65cb16daecc4d0d582
Piotr Mikołajczyk <piomiko41@gmail.com>
2023-01-19 13:19:54 +0100

    Migrate to ink!4 (#4)

--- shielder/public_token/lib.rs
+++ shielder/public_token/lib.rs
@@ -4,30 +4,35 @@
 /// Most basic PSP22 token.
 #[openbrush::contract]
 #[allow(clippy::let_unit_value)] // Clippy shouts about returning anything from messages.
-pub mod my_psp22 {
-    use ink_storage::traits::SpreadAllocate;
-    use openbrush::{contracts::psp22::*, traits::Storage};
+pub mod token {
+    use openbrush::{
+        contracts::psp22::{self, psp22::Internal, Data},
+        traits::Storage,
+    };
 
     #[ink(storage)]
-    #[derive(Default, SpreadAllocate, Storage)]
-    pub struct Contract {
+    #[derive(Default, Storage)]
+    pub struct Token {
         #[storage_field]
         psp22: Data,
     }
 
-    impl PSP22 for Contract {}
+    impl psp22::PSP22 for Token {}
 
-    impl Contract {
+    impl Token {
         /// Instantiate the contract with `total_supply` tokens of supply.
         ///
         /// All the created tokens will be minted to the caller.
         #[ink(constructor)]
         pub fn new(total_supply: Balance) -> Self {
-            ink_lang::codegen::initialize_contract(|instance: &mut Contract| {
-                instance
-                    ._mint_to(instance.env().caller(), total_supply)
-                    .expect("Should mint");
-            })
+            let mut instance = Self::default();
+
+            instance
+                .psp22
+                ._mint_to(Self::env().caller(), total_supply)
+                .expect("Should mint");
+
+            instance
         }
     }
 }
