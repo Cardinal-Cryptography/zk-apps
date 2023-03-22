dad2943f4be448ab88b78a4285891417d3132766
deuszx <mateusz.gorski@cardinals.cc>
2023-03-22 21:36:05 +0100

    Add e2e shielder tests to CI.

--- shielder/deploy/deploy.sh
+++ shielder/deploy/deploy.sh
@@ -43,9 +43,7 @@ get_timestamp() {
 }
 
 log_progress() {
-  bold=$(tput bold)
-  normal=$(tput sgr0)
-  echo "[$(get_timestamp)] [INFO] ${bold}${1}${normal}"
+  echo "[$(get_timestamp)] [INFO] ${1}"
 }
 
 random_salt() {
@@ -135,10 +133,7 @@ move_keys() {
 
 docker_cargo() {
   docker run --rm \
-    -u "${DOCKER_USER}" \
     -v "${PWD}":/code \
-    -v ~/.cargo/git:/usr/local/cargo/git \
-    -v ~/.cargo/registry:/usr/local/cargo/registry \
     --network host \
     --entrypoint /bin/sh \
     "${CARGO_IMAGE}" \
@@ -185,6 +180,7 @@ prefund_users() {
   for recipient in "${DAMIAN_PUBKEY}" "${HANS_PUBKEY}"; do
     transfer ${recipient}
   done
+  log_progress "✅ Test accounts prefunded with SNZERO tokens."
 }
 
 # Distribute TOKEN_PER_PERSON of TOKEN_A and TOKEN_B to DAMIAN and HANS.
@@ -196,13 +192,14 @@ distribute_tokens() {
       contract_call "--contract ${token} --message PSP22::transfer --args ${recipient} ${TOKEN_PER_PERSON} 0x00 --suri ${ADMIN}" 1>/dev/null
     done
   done
+  log_progress "✅ PSP22 tokens distributed"
 }
 
 deploy_shielder_contract() {
   cd "${SCRIPT_DIR}"/..
   SHIELDER_ADDRESS=$(contract_instantiate "--args ${MERKLE_LEAVES} --manifest-path contract/Cargo.toml" | jq -r '.contract')
   export SHIELDER_ADDRESS
-  log_progress "Shielder address: ${SHIELDER_ADDRESS}"
+  log_progress "✅ Shielder address: ${SHIELDER_ADDRESS}"
 }
 
 # Set allowance at TOKEN_ALLOWANCE on TOKEN_A and TOKEN_B from SHIELDER, from DAMIAN and HANS.
