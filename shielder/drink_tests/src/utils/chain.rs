333bc00b92e56a8d1255892f60f719cea6fd6004
Maksym <maksym.zub@cardinals.cc>
2024-03-20 16:24:55 +0100

    bump shielder to ink5-stable

--- shielder/drink_tests/src/utils/chain.rs
+++ shielder/drink_tests/src/utils/chain.rs
@@ -1,26 +1,27 @@
 use anyhow::Result;
-use drink::{runtime::MinimalRuntime, session::Session, AccountId32};
+use drink::sandbox_api::balance_api::BalanceAPI;
+use drink::{minimal::MinimalSandbox, session::Session, AccountId32};
 
 use crate::utils::ACCOUNT_INITIAL_AMOUNT;
 
 pub fn init_acc_with_balance(
-    session: &mut Session<MinimalRuntime>,
+    session: &mut Session<MinimalSandbox>,
     acc: &AccountId32,
 ) -> Result<()> {
     session
         .sandbox()
-        .mint_into(acc.clone(), ACCOUNT_INITIAL_AMOUNT)
+        .mint_into(&acc.clone(), ACCOUNT_INITIAL_AMOUNT)
         .unwrap();
     Ok(())
 }
 
-pub fn init_alice(session: &mut Session<MinimalRuntime>) -> Result<AccountId32> {
+pub fn init_alice(session: &mut Session<MinimalSandbox>) -> Result<AccountId32> {
     let res = AccountId32::new([2u8; 32]);
     init_acc_with_balance(session, &res)?;
     Ok(res)
 }
 
-pub fn init_bob(session: &mut Session<MinimalRuntime>) -> Result<AccountId32> {
+pub fn init_bob(session: &mut Session<MinimalSandbox>) -> Result<AccountId32> {
     let res = AccountId32::new([3u8; 32]);
     init_acc_with_balance(session, &res)?;
     Ok(res)
